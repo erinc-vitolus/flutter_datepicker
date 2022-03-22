02dc018456d19e59ea765eaeee1e9089ef1964ca
Erinç Argımak <erinc.argimak@vitolus.de>
2022-03-22 16:01:24 +0100

    Remove incomplete theme widget

--- lib/main.dart
+++ lib/main.dart
@@ -50,50 +50,60 @@ class _DatepickerPageState extends State<DatepickerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // Here we take the value from the DatepickerPage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        child: Theme(
-          data: Theme.of(context).copyWith(
-            textTheme: const TextTheme(
-                : TextStyle(
-              fontSize: 40,
-            )),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Choose a date:'),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 28),
-                  padding: const EdgeInsets.all(8),
-                ),
-                onPressed: () {
-                  _selectDate(context);
-                },
-                child: Text(DateFormat.yMMMEd().format(_date)),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Choose a date:'),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 28),
+                padding: const EdgeInsets.all(8),
               ),
-              const SizedBox(height: 40),
-              const Text(
-                'It has been',
+              onPressed: () {
+                _selectDate(context);
+              },
+              child: Text(DateFormat.yMMMEd().format(_date)),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'It has been',
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '${DateTime.now().difference(_date).inDays} days',
+              style: const TextStyle(
+                color: Colors.indigo,
+                fontWeight: FontWeight.bold,
+                fontSize: 48,
               ),
-              const SizedBox(height: 8),
-              Text(
-                '${DateTime.now().difference(_date).inDays} days',
-                style: const TextStyle(
-                  color: Colors.indigo,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 48,
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'since this date.',
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'since this date.',
+            ),
+          ],
         ),
       ),
     );
